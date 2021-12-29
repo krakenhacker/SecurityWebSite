fb1616c9a6d689e99f6a4eb4146ab610110ed9db
krakenhacker <george.sot@windowslive.com>
2021-12-29 13:14:39 +0200

    php mail function done

--- sumbit_form.php
+++ sumbit_form.php
@@ -1,3 +1,23 @@
 <?php
-echo "It Works!";
+header("Refresh:5; url=register.php"); 
+if(isset($_GET['submit'])){
+    $to = "george.sot@windowslive.com"; // this is your Email address
+    $from = $_GET['email']; // this is the sender's Email address
+    $firstName = $_GET['firstName'];
+    $lastName = $_GET['lastName'];
+    $phoneNumber = $_GET['phoneNumber'];
+    $comment = $_GET['comment'];
+    $time = $_GET['time'];
+    $subject = "Alarm Systems";
+    $subject2 = "Alarm Systems - Αντιγραφο";
+    $message = $first_name . " " . $last_name . " " . $phoneNumber . " Μήνυμα:" . "\n\n" . $comment . "\n\n" . "ωρα επικοινωνίας:" . $time;
+    $message2 = "Αντίγραφο του e-mail σας" . $first_name . " " . $last_name . " " . $phoneNumber . " Μήνυμα:" . "\n\n" . $comment . "\n\n" . "ωρα επικοινωνίας:" . $time;
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
